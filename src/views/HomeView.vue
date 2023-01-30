96bfe19d7a13442d38b5b366f2716941aedf35e5
Malte Werner <maltewerner27@gmail.com>
2023-01-30 09:40:11 +0100

    Get Data from API

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="content">
     <AngebotsSuche></AngebotsSuche>
-    <LiegeplatzListe></LiegeplatzListe>
+    <LiegeplatzListe v-if="Angebot"></LiegeplatzListe>
   </div>
 </template>
 
@@ -9,12 +9,32 @@
 // @ is an alias to /src
 import AngebotsSuche from '@/components/AngebotsSuche.vue'
 import LiegeplatzListe from '@/components/LiegeplatzListe.vue'
+import axios from 'axios'
 
 export default {
   name: 'HomeView',
   components: {
     AngebotsSuche,
     LiegeplatzListe
+  },
+  data() {
+  return{
+    Angebot: null,
+    LiegeplatzViewKey: 0
+  };
+  },
+
+  async created() {
+
+    var res = await axios.post('https://localhost:7082/api/Buchung/GetAngebote', {
+                                                                                    bootlaenge: 0,
+                                                                                    bootbreite: 0,
+                                                                                    boottiefgang: 0,
+                                                                                    startdatum: "2023-01-30T07:22:54.899Z",
+                                                                                    enddatum: "2023-01-30T07:22:54.899Z"});
+
+    this.Angebot = res.data;
+    console.log(this.Angebot);
   }
 }
 </script>
