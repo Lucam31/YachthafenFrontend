1130db4b1bed421c357ea8dca724e13260c110b3
Lucam31 <mueller.luca15@gmail.com>
2023-02-05 22:25:12 +0100

    Edit Buchungen Anzeige

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,10 +12,10 @@
             {{Buchung.m_BuchungsId}}
           </button>
         </h2>
-        <div :id="'panelsStayOpen-collapse'+Buchung.m_BuchungsId" class="accordion-collapse collapse" :aria-labelledby="'panelsStayOpen-heading'+Buchung.m_BuchungsId" onload="setPanelId($event, Buchung)">
-          <div class="accordion-body">
+        <div :id="'panelsStayOpen-collapse'+Buchung.m_BuchungsId" class="accordion-collapse collapse" :aria-labelledby="'panelsStayOpen-heading'+Buchung.m_BuchungsId">
+          <div class="accordion-body flex-accordion">
             <strong>{{Buchung.m_Startdatum}} bis zum {{Buchung.m_Enddatum}}</strong> Auf Liegeplatz {{ Buchung.m_Liegeplatz.m_Bezeichnung }} mit Boot {{ Buchung.m_Boot.m_Name }}
-            <button type="submit" class="btn btn-primary cancelButton" v-on:click.prevent="onBootAnlegen">Stornieren</button>
+            <button type="submit" class="input-group-text cancelButton" v-on:click.prevent="onBootAnlegen">Stornieren</button>
           </div>
         </div>
       </div>
@@ -30,13 +30,13 @@
     <div class="accordion mb-5" id="accordionPanelsStayOpenExample">
       <div class="accordion-item" v-for="alteBuchung in alteBuchungen" :key="alteBuchung.m_BuchungsId">
         <h2 class="accordion-header" id="panelsStayOpen-headingOne">
-          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="false" aria-controls="panelsStayOpen-collapseOne">
+          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" :data-bs-target="'#panelsStayOpen-collapseOne'+alteBuchung.m_BuchungsId" aria-expanded="false" :aria-controls="'panelsStayOpen-collapseOne'+alteBuchung.m_BuchungsId">
             {{alteBuchung.m_BuchungsId}}
           </button>
         </h2>
-        <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingOne">
+        <div :id="'panelsStayOpen-collapseOne'+alteBuchung.m_BuchungsId" class="accordion-collapse collapse" :aria-labelledby="'panelsStayOpen-headingOne'+alteBuchung.m_BuchungsId">
           <div class="accordion-body">
-            <strong>{{alteBuchung.m_Startdatum}} bis zum {{alteBuchung.m_Enddatum}}</strong> Auf Liegeplatz {{ alteBuchung.m_Liegeplatz.m_Bezeichnung }} mit Boot {{ alteBuchung.m_Boot.m_Name }}
+            <strong>{{alteBuchung.m_Startdatum}} bis zum {{alteBuchung.m_Enddatum}}</strong> Auf Liegeplatz {{ alteBuchung.m_Bezeichnung }} mit Boot {{ alteBuchung.m_Boot }}
           </div>
         </div>
       </div>
@@ -49,15 +49,15 @@
   </div>
   <div class="itemList collapse show" id="boote">
     <div class="accordion mb-5" id="accordionPanelsStayOpenExample">
-      <div class="accordion-item" v-for="Boot in Boote" :key="Boot.registrierungs_id">
-        <h2 class="accordion-header" id="panelsStayOpen-headingOne">
-          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="false" aria-controls="panelsStayOpen-collapseOne">
-            {{Boot.registrierungs_id}}
+      <div class="accordion-item" v-for="Boot in Boote" :key="Boot.m_RegistrierungsId">
+        <h2 class="accordion-header" :id="'panelsStayOpen-heading'+Boot.m_RegistrierungsId+Boot.m_Name">
+          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" :data-bs-target="'#panelsStayOpen-collapse'+Boot.m_RegistrierungsId+Boot.m_Name" aria-expanded="false" :aria-controls="'panelsStayOpen-collapse'+Boot.m_RegistrierungsId+Boot.m_Name">
+            {{Boot.m_RegistrierungsId}}
           </button>
         </h2>
-        <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingOne">
+        <div :id="'panelsStayOpen-collapse'+Boot.m_RegistrierungsId+Boot.m_Name" class="accordion-collapse collapse" :aria-labelledby="'panelsStayOpen-heading'+Boot.m_RegistrierungsId+Boot.m_Name">
           <div class="accordion-body">
-            <strong>{{Boot.name}} ist {{Boot.laenge}} Meter lang</strong>
+            <strong>{{Boot.m_Name}} ist {{Boot.m_Laenge}} Meter lang und {{ Boot.m_Breite }} Meter breit</strong>
           </div>
         </div>
       </div>
@@ -82,7 +82,10 @@
       this.$router.push({name: "login"});
     }
       this.Buchungen = await (await axios.get(APIURLService.getAPIUrl()+'/api/Kunden/GetBuchungenFromKunde?kundenId=' + this.$store.getters.getKundenId)).data;
-      this.Boote = [{registrierungs_id: "abc", name: "Boot1", laenge: "69"}];
+      this.alteBuchungen = [{m_BuchungsId: "abc", m_Bezeichnung: "LP3", m_Boot: "69", m_Startdatum: "23.01.2023", m_Enddatum: "01.02.2023"},{m_BuchungsId: "def", m_Bezeichnung: "LP4", m_Boot: "79", m_Startdatum: "23.01.2023", m_Enddatum: "01.02.2023"}];
+      // this.alteBuchungen = await (await axios.get(APIURLService.getAPIUrl()+'/api/Kunden/GetVergangeneBuchungenFromKunde?kundenId=' + this.$store.getters.getKundenId)).data;
+      this.Boote = await (await axios.get(APIURLService.getAPIUrl()+'/api/Kunden/GetBooteFromKunde?kundenId='+ this.$store.getters.getKundenId)).data;
+      console.log(this.Boote)
       
     },
     data() {
@@ -92,7 +95,6 @@
     },
     methods:{ 
       changeSign(el) {
-        console.log(el);
         if (el.target.innerHTML == "+") {
           el.target.innerHTML = "-"
         } else {
@@ -109,9 +111,15 @@
   button{
     margin: 0;
   }
+  .flex-accordion{
+    display: flex;
+    flex-wrap: wrap;
+  }
   .cancelButton{
-    position: relative;
-    float: right;
+    position: absolute;
+    right: 0;
+    transform: translate(-50%, 0);
+    margin-right: 10px;
   }
   .flex{
     display: flex;
